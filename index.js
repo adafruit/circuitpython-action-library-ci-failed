f3ddcc61c6400c826565273738fa2598e0e5e2d7
Scott Shawcroft <scott@tannewt.org>
2021-04-13 08:32:38 -0700

    remove prints. fix link

--- index.js
+++ index.js
@@ -2,23 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-  console.log(github.context);
-
   const token = core.getInput('token');
   const octokit = github.getOctokit(token);
 
   const repo = github.context.payload.repository;
-  const issue = github.context.payload.workflow_run.pull_requests[0];
-  console.log(repo);
-  console.log(issue);
-
+  const workflow_run = github.context.payload.workflow_run;
+  const issue = workflow_run.pull_requests[0];
 
   // Use a reaction to track if we've replied already. 201 is returned
   // the first time we set it. 200 if it was already created. This saves
   // us from having to look through all of the existing comments.
 
   try {
-    console.log(issue.number, repo.owner.login, repo.name);
     const result = await octokit.reactions.createForIssue({
         issue_number: issue.number,
         owner: repo.owner.login,
@@ -31,7 +26,7 @@ async function run() {
         issue_number: issue.number,
         owner: repo.owner.login,
         repo: repo.name,
-        body: "👋 Thanks for this pull request! Unfortunately, it looks like the automated continuous integration (CI) [test(s) failed](). These can be tricky to fix so we've written a guide on how to fix them locally. It has pages about [running pre-commit locally](https://learn.adafruit.com/creating-and-sharing-a-circuitpython-library/check-your-code) and another about [building the docs locally with sphinx](https://learn.adafruit.com/creating-and-sharing-a-circuitpython-library/sharing-our-docs-on-readthedocs). Thanks for contributing to CircuitPython! If you have more questions, feel free to join [the Adafruit Discord](https://adafru.it/discord) and post in #circuitpython-dev."
+        body: "👋 Thanks for this pull request! Unfortunately, it looks like the automated continuous integration (CI) [test(s) failed](" + workflow_run.html_url + "). These can be tricky to fix so we've written a guide on how to fix them locally. It has pages about [running pre-commit locally](https://learn.adafruit.com/creating-and-sharing-a-circuitpython-library/check-your-code) and another about [building the docs locally with sphinx](https://learn.adafruit.com/creating-and-sharing-a-circuitpython-library/sharing-our-docs-on-readthedocs). Thanks for contributing to CircuitPython! If you have more questions, feel free to join [the Adafruit Discord](https://adafru.it/discord) and post in #circuitpython-dev."
       });
     } else {
       console.log("Already commented");
